dd542b08236dbbc6d8ccc3bf06ac55c2a8d26558
Bruno Martins <bgcngm@gmail.com>
2018-02-23 10:21:34 +0000

    yellowstone: Remove wifi{con,log}d from device makefiles
     * These package are being built globally.
    
    Change-Id: I70aaf469acc29ded3cf5bb24ab112a76b36a7835

--- device.mk
+++ device.mk
@@ -230,8 +230,6 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service.legacy \
     hostapd \
     libwpa_client \
-    wificond \
-    wifilogd \
     wpa_supplicant \
     wpa_supplicant.conf
 
